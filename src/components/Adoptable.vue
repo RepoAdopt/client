3f73414ede8c4cb2df920b26632e8217f64dac97
Niek125 <niek.sleddens@gmail.com>
2021-03-08 15:05:50 +0100

    xards

---
+++ src/components/Adoptable.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-card :header="name" shadow="hover" class="card">
+    <p>{{ description }}</p>
+  </el-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'adoptable',
+  props: {
+    name: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    repository: {
+      type: String,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.card {
+	margin: 16px;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,77 @@
 <template>
   <el-row>
     <el-col :span="6">left</el-col>
-    <el-col :span="12">mid</el-col>
+
+    <el-col :span="12">
+      <ul class="infinite-list" v-infinite-scroll="load">
+        <li v-for="(adoptable, index) in adoptables" :key="adoptable.name + index">
+          <Adoptable :name="adoptable.repository" :description="adoptable.description" />
+        </li>
+      </ul>
+    </el-col>
+
     <el-col :span="6">right</el-col>
   </el-row>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import Adoptable from '@/components/Adoptable.vue';
+
+export default defineComponent({
   name: 'Dashboard',
-  components: {},
-};
+  components: { Adoptable },
+  data() {
+    return {
+      adoptables: [
+        {
+          repository: 'RepoAdopt/client',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce suscipit condimentum est nec malesuada. Donec sollicitudin interdum turpis, vel mattis metus sodales sit amet.',
+        },
+      ],
+    };
+  },
+  methods: {
+    load() {
+      const repo = {
+          repository: 'RepoAdopt/client',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce suscipit condimentum est nec malesuada. Donec sollicitudin interdum turpis, vel mattis metus sodales sit amet.',
+        }
+
+      this.adoptables.push(repo);
+      this.adoptables.push(repo);
+      this.adoptables.push(repo);
+      this.adoptables.push(repo);
+      console.log(this.adoptables.length);
+    },
+  },
+});
 </script>
+
+<style lang="scss" scoped>
+.infinite-list {
+  margin: 0 10px;
+  overflow: auto;
+  max-height: 90vh;
+
+  &::-webkit-scrollbar-track {
+    display: none;
+  }
+
+  &::-webkit-scrollbar {
+    width: 12px;
+  }
+
+  &::-webkit-scrollbar-button {
+    display: none;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: grey;
+    border-radius: 16px;
+  }
+}
+</style>
