c2328b738d644b21666c890fe85468b46d3a811b
Niek125 <niek.sleddens@gmail.com>
2021-03-15 15:21:41 +0100

    styling

--- src/components/Adoptable.vue
+++ src/components/Adoptable.vue
@@ -1,5 +1,10 @@
 <template>
-  <el-card :header="repository" shadow="hover" class="card">
+  <el-card shadow="hover" class="card">
+    <template #header>
+      <h1>
+        <b>{{ repository }}</b>
+      </h1>
+    </template>
     <p v-if="description?.length">{{ description }}</p>
     <el-divider v-if="description?.length && readme?.length" />
     <vue3-markdown-it v-if="readme?.length" :source="readme" />
@@ -28,5 +33,9 @@ export default defineComponent({
 <style lang="scss" scoped>
 .card {
   margin: 16px;
+}
+
+h1 {
+  font-size: 2rem;
 }
 </style>
