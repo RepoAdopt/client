e4b741af4b2709a1c671187458a735254ec62929
Niek125 <niek.sleddens@gmail.com>
2021-03-22 14:30:11 +0100

    shell

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,5 +1,14 @@
 <template>
-  login
+  <el-form>
+    <h1>Sign in</h1>
+    <el-form-item label="Email">
+      <el-input />
+    </el-form-item>
+    <el-form-item label="Password">
+      <el-input />
+    </el-form-item>
+    <el-button>Sign in with GitHub</el-button>
+  </el-form>
 </template>
 
 <script lang="ts">
