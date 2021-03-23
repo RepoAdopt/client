91a2d221f5625b111b6faabe002082562a63788c
Niek125 <niek.sleddens@gmail.com>
2021-03-23 09:18:36 +0100

    authentication with github

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,33 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Api from '@/api';
 
 import routes from './routes';
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.redirectedFrom?.path === '/auth/gh') {
+    Api.post(
+      '/signin',
+      {},
+      {
+        params: {
+          client_id: process.env.VUE_APP_CLIENT_ID,
+          code: to.redirectedFrom.query.code,
+        },
+      }
+    ).then((res) => {
+      console.log(res);
+    });
+
+    delete to.query.code;
+    to.fullPath = to.fullPath.replace(/\?code(.*)/, '');
+  }
+  next();
 });
 
 export default router;
