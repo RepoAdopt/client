00bdcafb2a95b4432508d6d09df3b471a4f8ef2e
BTaapken <beau@lioncode.nl>
2021-03-29 15:27:33 +0200

    * able to search adoptables

--- src/components/OwnRepositories.vue
+++ src/components/OwnRepositories.vue
@@ -2,12 +2,12 @@
   <el-col class="padding">
     <h1>My repositories</h1>
     <el-input
-        placeholder="Please input"
-        v-model="input"
-        clearable>
-    </el-input>
-    <ul class="infinite-list" v-infinite-scroll="load" v-if="adoptables">
-      <li class="own-adoptables" v-for="(adoptable, index) in adoptables" :key="`adoptable:${index}-${adoptable.repository}`">
+        placeholder="Search for your adoptables"
+        v-model="search"
+        clearable
+    />
+    <ul id="own-adoptable-list" v-if="adoptables">
+      <li class="own-adoptables" v-for="(adoptable, index) in filteredList" :key="`adoptable:${index}-${adoptable.repository}`">
         <h2>{{ adoptable.repository }}</h2>
       </li>
     </ul>
@@ -22,10 +22,18 @@ export default defineComponent({
   name: "OwnRepositories",
   data() {
     return {
-      search: null,
+      search: "",
     };
   },
-  computed: { ...mapGetters('adoptables', ['adoptables']) },
+  computed: {
+    ...mapGetters('adoptables', ['adoptables']),
+
+    filteredList: function() {
+      return this.adoptables.filter(adoptable => {
+        return adoptable.repository.toLowerCase().includes(this.search.toLowerCase())
+      })
+    }
+  },
   methods: {
     ...mapActions('adoptables', ['load']),
   },
