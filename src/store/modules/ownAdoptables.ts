c325e6c880a493dda9a266a2368fc08006d2ef30
BTaapken <beau@lioncode.nl>
2021-03-30 11:40:00 +0200

    * Added ts file so own adoptables can be gotten

---
+++ src/store/modules/ownAdoptables.ts
@@ -0,0 +1,72 @@
+import gql from 'graphql-tag';
+
+import Apollo from '@/apollo';
+import Octokit from '@/octokit';
+
+interface Adoptable {
+  repository: string;
+  description: string;
+}
+
+interface State {
+  adoptables: Array<Adoptable>;
+}
+
+const state = () => ({
+  adoptables: [],
+});
+
+const getters = {
+  adoptables: (state: State) => {
+    return state.adoptables;
+  },
+};
+
+const actions = {
+  load(root: { commit: (mutation: string, params?: any) => void; state: State }) {
+
+    root.commit('startFetch');
+
+    Apollo.query({
+      query: gql`
+        query($page: Int!, $limit: Int!) {
+          adoptable(page: $page, limit: $limit) {
+            repository
+          }
+        }
+      `,
+      variables: { page: root.state.page, limit: process.env.VUE_APP_PAGINATION_LIMIT },
+    })
+      .then((result) => {
+        result.data.adoptable.forEach((adoptable: Adoptable) => {
+          const [owner, repo] = adoptable.repository.split('/', 2);
+          root.commit('addAdoptable', { adoptables: adoptable });
+        });
+      })
+      .catch((err) => {
+        setTimeout(function() {
+          root.commit('finishFetch');
+        }, 1000);
+      });
+  },
+};
+
+const mutations = {
+  addAdoptable(state: State, params: { adoptables: Array<Adoptable> | Adoptable }) {
+    const { adoptables } = params;
+
+    if (Array.isArray(adoptables)) {
+      state.adoptables.push(...adoptables);
+    } else {
+      state.adoptables.push(adoptables);
+    }
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
